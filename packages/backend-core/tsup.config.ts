546d700dc263211dd62eb7adc6105d7db1d89558
Azaz Ahamed Zoha <ahamed.zoha@gmail.com>
2025-10-03 23:44:03 +0600

    added declaration map

--- packages/backend-core/tsup.config.ts
+++ packages/backend-core/tsup.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm"],
-  dts: true,
+  dts: {
+    compilerOptions: {
+      declarationMap: true,
+    },
+  },
   splitting: false,
   sourcemap: true,
   clean: true,
