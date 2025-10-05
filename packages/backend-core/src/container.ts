5e91d9ba64575101fb57dcf6bfa84f1ee5315948
Azaz Ahamed Zoha <ahamed.zoha@gmail.com>
2025-10-05 15:21:23 +0600

    updated be container jsdocs

--- packages/backend-core/src/container.ts
+++ packages/backend-core/src/container.ts
@@ -13,7 +13,32 @@ export interface IServiceContainer {
   authService: AuthService;
   userService: UserService;
 }
-
+/**
+ * BackendContainer is a class that initializes the database connection and the services.
+ * It is used to inject the dependencies into the controllers and routes.
+ * @param config - The configuration for the database and the services.
+ * @returns The BackendContainer instance.
+ * @example
+ * const container = new BackendContainer({
+ *   host: "localhost",
+ *   port: 5432,
+ *   username: "postgres",
+ *   password: "password",
+ *   database: "shonchoy_auth",
+ *   accessToken: {
+ *     secret: "your-super-secure-access-token-secret-here-at-least-32-chars",
+ *     expiresIn: "15m",
+ *   },
+ *   refreshToken: {
+ *     secret: "your-super-secure-refresh-token-secret-here-at-least-32-chars",
+ *     expiresIn: "7d",
+ *   },
+ * });
+ *
+ * await container.initialize();
+ * const app = createApp(container);
+ * app.listen(3000);
+ */
 export class BackendContainer implements IServiceContainer {
   private _dataSource: DataSource;
   private _authService: AuthService;
